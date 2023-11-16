fdea84d72fef58b38c9e9832f7c8af7d755c0328
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-11-16 21:55:39 +0700

    fix: fix layout footer

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -6,8 +6,8 @@ import motchill from '../../assets/images/motchill.png';
 
 const Footer = () => {
   return (
-    <div className="w-full flex justify-center items-center text-white bg-black py-20">
-      <div className="w-full flex flex-col px-10 text-left gap-5 md:grid grid-cols-3 lg:w-3/5">
+    <div className="w-full flex flex-col justify-center items-center text-white bg-black pt-20 pb-10">
+      <div className="w-full flex flex-col px-10 text-left gap-5 md:grid grid-cols-3 lg:w-4/5">
         <div>
           <img src={motchill} alt="" className="w-48" />
           <Label className="text-justify">
@@ -41,6 +41,11 @@ const Footer = () => {
           ))}
         </div>
       </div>
+      <hr className="w-full border-white my-10" />
+      <p className="text-xl border-dotted text-[#A3765D]">
+        Tui tên là Kim Cương, vì ghét xem film có quảng cáo nên tui code ra web
+        này hehe
+      </p>
     </div>
   );
 };
